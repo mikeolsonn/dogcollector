8287baa62ad914beb9b7d1c79d113bde94b94b3a
Mike Olson <mikeolsonn97@gmail.com>
2021-02-19 13:37:20 -0500

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+class Dog:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+dogs = [
+    Dog('Otto', 'Bichon', 'frisky little marshmallow', 7),
+    Dog('Zeus', 'Puggle', 'good boy', 10),
+    Dog('Martha', 'Yellow Lab', 'the most loyal', 5),
+]
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'cats/index.html', {'dogs': dogs})
