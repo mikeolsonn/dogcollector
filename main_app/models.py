ef47fba1e20e12a7bf7d21b37900eb387758f57d
Mike Olson <mikeolsonn97@gmail.com>
2021-02-21 19:56:35 -0500

    adds dog model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=250)
+    breed = models.CharField(max_length=250)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
