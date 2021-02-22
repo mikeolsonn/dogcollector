c55b080235bdf6d3a818f77d36dbcb7a8c36ef25
Mike Olson <mikeolsonn97@gmail.com>
2021-02-21 21:32:09 -0500

    adds links for detail pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-dogs = [
-    Dog('Otto', 'Bichon', 'frisky little marshmallow', 7),
-    Dog('Zeus', 'Puggle', 'good boy', 10),
-    Dog('Martha', 'Yellow Lab', 'the most loyal', 5),
-]
+from .models import Dog
 
 # Create your views here.
 
@@ -24,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
