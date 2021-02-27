db0cb052b094c5f3f0579b0fc0f73219bd7a58af
Mike Olson <mikeolsonn97@gmail.com>
2021-02-26 20:03:38 -0500

    adds post update and delete functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here.
@@ -18,4 +19,16 @@ def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     # return a call to render the detail.html and context dict
     return render(request, 'dogs/detail.html', {'dog': dog})
-    
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    # success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
